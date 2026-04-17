4eaafbcfee7035130d68caf846e45c3aeb801e7e
Lynx <lynx@koda.dev>
2026-04-17 20:11:34 +0200

    fix: TermuxInstaller public, BuildConfig import, Logger API

--- app/src/main/java/com/termux/app/TermuxApplication.java
+++ app/src/main/java/com/termux/app/TermuxApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.termux.shared.logger.Logger;
 import com.termux.shared.termux.TermuxBootstrap;
 import com.termux.shared.termux.crash.TermuxCrashUtils;
-import com.termux.shared.termux.settings.preferences.TermuxAppSharedPreferences;
 
 public class TermuxApplication extends Application {
 
@@ -16,13 +15,13 @@ public class TermuxApplication extends Application {
         super.onCreate();
 
         // Set package variant from BuildConfig
-        TermuxBootstrap.setPackageVariant(BuildConfig.TERMUX_PACKAGE_VARIANT);
+        TermuxBootstrap.setPackageVariant(com.termux.BuildConfig.TERMUX_PACKAGE_VARIANT);
 
         // Set crash handler
         TermuxCrashUtils.setCrashHandler(this);
 
         // Set log level
-        Logger.setDefaultLogLevel(this, Logger.LOG_LEVEL_NORMAL);
+        Logger.setLogLevel(null, Logger.LOG_LEVEL_NORMAL);
 
         Logger.logInfo(LOG_TAG, "Koda application started");
     }
